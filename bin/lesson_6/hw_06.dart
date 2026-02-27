d832aa4ed24d433172b8c13e8725981ce079bc31
beknazar7 <beknazard105@gmail.com>
2026-02-28 00:05:49 +0600

    lesson 6

--- bin/lesson_6/hw_06.dart
+++ bin/lesson_6/hw_06.dart
@@ -1,6 +1,29 @@
 import 'person.dart';
 
-void main(){
-  var beka = Person('Beknazar Denizbekov', 18, false);
-  beka.introduce();
+void main() {
+  print('--- Teacher Section ---');
+  var teacher = Teacher('John Brown', 40, true, 5);
+  teacher.introduce();
+
+  print('\n--- Students Section ---');
+  
+  var student1 = Student('Adam White', 17, false, {
+    Subject.math: 90,
+    Subject.physics: 85,
+    Subject.english: 92,
+  });
+  
+  var student2 = Student('Beknazar Denizbekov', 18, false, {
+    Subject.math: 100,
+    Subject.history: 95,
+    Subject.biology: 90,
+  });
+
+  student1.introduce();
+  student1.showMarks();
+  
+  print('---');
+  
+  student2.introduce();
+  student2.showMarks();
 }
--- bin/lesson_6/person.dart
+++ bin/lesson_6/person.dart
@@ -74,31 +74,3 @@ class Teacher extends Person {
     print('Salary: ${calculateSalary().toStringAsFixed(2)}');
   }
 }
-
-void main() {
-  print('--- Teacher Section ---');
-  var teacher = Teacher('John Brown', 40, true, 5);
-  teacher.introduce();
-
-  print('\n--- Students Section ---');
-  
-  var student1 = Student('Adam White', 17, false, {
-    Subject.math: 90,
-    Subject.physics: 85,
-    Subject.english: 92,
-  });
-  
-  var student2 = Student('Beknazar Denizbekov', 18, false, {
-    Subject.math: 100,
-    Subject.history: 95,
-    Subject.biology: 90,
-  });
-
-  student1.introduce();
-  student1.showMarks();
-  
-  print('---');
-  
-  student2.introduce();
-  student2.showMarks();
-}
